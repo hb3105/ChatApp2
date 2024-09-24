8ca310b0b8cb4a69e72139fd6aaa8ad3bce22556
hb3105 <harsha.bandra@gmail.com>
2024-09-24 12:49:27 +0800

    chat room list
    shows available chatrooms
    fixed logout handling and empty node.

--- ChatApp/chat/views.py
+++ ChatApp/chat/views.py
@@ -1,10 +1,10 @@
-# chat/views.py
 from django.shortcuts import render
-
+from .models import Room
 
 def index(request):
-    return render(request, "chat/index.html")
-
+    rooms = Room.objects.all()  # Fetch all rooms from the database
+    return render(request, "chat/index.html", {"rooms": rooms})
 
 def room(request, room_name):
+    room, created = Room.objects.get_or_create(name=room_name)  # Create room if it doesn't exist
     return render(request, "chat/room.html", {"room_name": room_name})
--- ChatApp/users/urls.py
+++ ChatApp/users/urls.py
@@ -5,6 +5,7 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
+    path("", views.dashboard, name="dashboard"), #empty path https:/127.0.0.1:8000
     path("accounts/", include("django.contrib.auth.urls")),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("register/", views.register, name="register"),
