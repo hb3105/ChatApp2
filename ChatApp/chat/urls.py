451d57cb4bb1d30c6d93cf02f4edaab565c7bce7
hb3105 <harsha.bandra@gmail.com>
2024-09-25 21:24:49 +0800

    updated urls

--- ChatApp/chat/urls.py
+++ ChatApp/chat/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = 'chat'
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("index/", views.index, name="index"),
     path("<str:room_name>/", views.room, name="room"),
 ]
